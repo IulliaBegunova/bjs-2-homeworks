05b2e8bbfade272a7c168d41879f9408d224aaad
Iuliia Begunova <begunova-julia@yandex.ru>
2022-10-01 19:11:14 +0300

    Три задачи

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,59 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
-}
-
+	let min = Infinity;
+	let max = -Infinity;
+	let sum = 0;
+	let avg = 0;
+	// Ваш код
+	for (let i = 0; i < arr.length; i++) {
+		if (max < arr[i]) {
+			max = arr[i];
+		};
+		if (min > arr[i]) {
+			min = arr[i];
+		};
+		sum += arr[i];
+	}
+	avg = sum / arr.length;
+	avg = Number(avg.toFixed(2));
+	return { min, max, avg };
+};
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
-  return sum;
-}
-
+	let sum = 0;
+	// Ваш код
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+	};
+	return sum;
+};
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
-  return max;
-}
-
+	let max = -Infinity;
+	let min = Infinity;
+	// Ваш код
+	for (let j = 0; j < arrOfArr.length; j++) {
+		if (max < func(arrOfArr[j])) {
+			max = func(arrOfArr[j]);
+		};
+		if (min > func(arrOfArr[j])) {
+			min = func(arrOfArr[j]);
+		};
+	};
+	return max;
+};
 // Задание 3
 function worker2(arr) {
-  // Ваш код
-}
+	let min = Infinity;
+	let max = -Infinity;
+  
+	for (let i = 0; i < arr.length; i++) {
+	  if (arr[i] < min) {
+		min = arr[i];
+	  }
+	  if (arr[i] > max) {
+		max = arr[i];
+	  }
+	}
+  
+	return max - min;
+  }
